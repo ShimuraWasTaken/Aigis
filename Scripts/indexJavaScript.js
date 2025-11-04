0abe145a511d70ebeb583c4fdf08adf8dceb2946
ShimuraWasTaken <ggatuno247@gmail.com>
2025-11-04 14:47:49 -0800

    v1
    First Version just for test

---
+++ Scripts/indexJavaScript.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("myAudio");
+document.getElementById("myAudio").volume = 0.2; 
+x.play();
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
