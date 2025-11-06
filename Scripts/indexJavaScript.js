dbf3b3de81a5599bac2126543b9cd201f98dfabb
ShimuraWasTaken <ggatuno247@gmail.com>
2025-11-05 20:05:46 -0800

    v3
    almost finish

--- Scripts/indexJavaScript.js
+++ Scripts/indexJavaScript.js
@@ -1,10 +1,12 @@
 var x = document.getElementById("myAudio");
 document.getElementById("myAudio").volume = 0.2; 
-x.play();
 function playAudio() { 
   x.play(); 
 } 
 
 function pauseAudio() { 
   x.pause(); 
-} 
+}
+function btn1() {
+  document.getElementById("btn1").href = ".";
+}
